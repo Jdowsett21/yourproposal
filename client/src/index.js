54be556d09a199c381502163947a3d9162f6a877
James Dowsett-Cooper <jamesdowsett-cooper@Jamess-MacBook-Pro.local>
2021-02-10 13:50:23 -0500

    d

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './App.scss';
-import ReactGA from 'react-ga';
 ReactDOM.render(
   <React.StrictMode>
     <App />
