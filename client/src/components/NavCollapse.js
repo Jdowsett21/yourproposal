4af51621612b093a073389a33f7ddbd211a4bafe
James Dowsett-Cooper <jamesdowsett-cooper@Jamess-MacBook-Pro.local>
2021-02-12 14:47:40 -0500

    d

--- client/src/components/NavCollapse.js
+++ client/src/components/NavCollapse.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setNavOpen } from '../actions/InViewActions';
-import hello from '../img/svg/navBarButton.png';
+import hello from '../img/svg/navBarButton.svg';
 import logo from '../img/logo.png';
 import logoweb from '../img/logo.webp';
 import useWindowDimensions from '../utils/ScreenWidth';
