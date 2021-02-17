d48a0fe33cede235105ae528927fbb4f72a1955c
James Dowsett-Cooper <jamesdowsett-cooper@Jamess-MacBook-Pro.local>
2021-02-17 14:25:52 -0500

    d

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,6 @@
 const withImages = require('next-images');
 const withCss = require('@zeit/next-css');
+const { withXDN, withServiceWorker } = require('@xdn/next/config');
 const withSass = require('@zeit/next-sass');
-module.exports = withImages(withSass(withCss()));
+
+module.exports = withXDN(withServiceWorker(withImages(withSass(withCss()))));
