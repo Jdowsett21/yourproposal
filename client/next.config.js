1208c5e0bf0f42aac4e407c35822f15244093154
James Dowsett-Cooper <jamesdowsett-cooper@Jamess-MacBook-Pro.local>
2021-02-18 15:52:57 -0500

    d

--- client/next.config.js
+++ client/next.config.js
@@ -4,21 +4,5 @@ const withSass = require('@zeit/next-sass');
 
 module.exports = withImages(
   { responsive: { adapter: require('responsive-loader/sharp') } },
-  withSass({ minified: true }, withCss()),
-  {
-    exportPathMap: async function (
-      defaultPathMap,
-      { dev, dir, outDir, distDir, buildId }
-    ) {
-      return {
-        '/': { page: '/' },
-        '/services': { page: '/services' },
-        '/process': { page: '/process' },
-        '/services': { page: '/services' },
-        '/services': { page: '/services' },
-        '/services': { page: '/services' },
-        '/services': { page: '/services' },
-      };
-    },
-  }
+  withSass({ minified: true }, withCss())
 );
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -10,6 +10,7 @@ import Footer from '../components/Footer';
 import { StaticRouter as Router } from 'react-router-dom';
 import { useMediaQuery } from '../utils/ScreenWidth';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 const MyApp = ({ Component, pageProps }) => {
   const mediaQuery = useMediaQuery(1100);
 
@@ -17,6 +18,9 @@ const MyApp = ({ Component, pageProps }) => {
     ReactGA.initialize('UA-189418529-1');
     ReactGA.pageview(window.location.pathname + window.location.search);
   });
+  const canURL = useRouter().pathname;
+  console.log('🚀 ~ file: _app.js ~ line 22 ~ MyApp ~ canURL', canURL);
+
   const store = useStore(pageProps.initialReduxState);
   return (
     <Router>
