93773fc2ec6498f7fab996b2887e937ef60b92b0
James Dowsett-Cooper <jamesdowsett-cooper@Jamess-MacBook-Pro.local>
2021-02-17 20:56:37 -0500

    d

--- client/server.js
+++ client/server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
-
+const LRUCache = require('lru-cache');
 const ssrCache = new LRUCache({
   max:
     100 *
