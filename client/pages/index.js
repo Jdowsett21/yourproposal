c5ee927c520dd1a8fbbb4e7b78b523315633c74c
James Dowsett-Cooper <jamesdowsett-cooper@Jamess-MacBook-Pro.local>
2021-02-17 21:19:44 -0500

    d

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,7 @@
 import React, { useEffect } from 'react';
 import Home from '../pages/Home';
-import dynamic from 'next/dynamic';
-const DynamicComponent = dynamic(() => import('./Home'));
 function App() {
-  return <DynamicComponent />;
+  return <Home />;
 }
 
 export default App;
